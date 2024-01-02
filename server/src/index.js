2df927dcd8105abaab50e35e797db5359fb290f2
Juan Feris <feris.juan@gmail.com>
2024-01-02 14:56:40 -0500

    connect to mongo

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,24 @@
+require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
+app.use(bodyParser.json())
+app.use(authRoutes)
+
+const mongoURI = process.env.MONGO_URI
+mongoose.connect(mongoURI, {
+  retryWrites: true,
+})
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to mongo', err)
+})
+
 
 app.get('/', (req, res) => {
 
